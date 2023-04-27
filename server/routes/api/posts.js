0356fb5bc480f12a4a456a71c1b7d211f3910f86
William Zhu <wxz472@case.edu>
2023-04-26 23:29:11 -0400

    initial backend

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+const mongodb = require('mogodb');
+
+const router = express.Router();
+
+//Get Posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+//Add Posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+
+//Delete Posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://wxz472:bluehorse1@cluster0.tdcgywc.mongodb.net/test',
+    {useNewUrlParser: true});
+
+    return client.db('vue_express').collection('posts');
+}
+
+
+module.exports = router;
